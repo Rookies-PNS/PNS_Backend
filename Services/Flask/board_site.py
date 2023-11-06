5fdbf28284629e5ac1f4a9beaa17330b1116023f
taktakgyun <vbmrkk@naver.com>
2023-11-06 20:39:52 +0900

    🚚 chore: Move to Views
    templates와 static 을 Views안으로 이동

--- Services/Flask/board_site.py
+++ Services/Flask/board_site.py
@@ -4,7 +4,12 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from Services import get_secrets_key
 
 # APP init
-app = Flask(__name__, static_url_path="/static", static_folder="static")
+app = Flask(
+    __name__,
+    static_url_path="/Views/static",
+    static_folder="Views/static",
+    template_folder="Views/templates",
+)
 app.secret_key = get_secrets_key()
 
 
@@ -12,6 +17,15 @@ app.secret_key = get_secrets_key()
 @app.route("/")
 def home():
     return render_template("home/home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        userID = request.form["userID"]
+        password = request.form["password"]
+
+    return render_template("auth/login.html")
 
 
 if __name__ == "__main__":
