2cee0ec00ffdeba760068eb5c17724f2cbf178b3
taktakgyun <vbmrkk@naver.com>
2023-11-06 22:47:06 +0900

    ✅ test: Add LoginUser
    로그인 잘될 때, 되면 안될 때, 테스트 추가

--- Tests/Applications/Usecase/test_user_services.py
+++ Tests/Applications/Usecase/test_user_services.py
@@ -5,7 +5,7 @@ from typing import List
 
 from Commons import UserId, Uid, Password
 from Domains.Entities import UserVO, User
-from Applications.Usecases import CreateUser
+from Applications.Usecases import CreateUser, LoginUser
 from Applications.Results import (
     Fail,
     Fail_CheckUser_IDNotFound,
@@ -33,6 +33,8 @@ class test_create_user(unittest.TestCase):
         print("\t", sys._getframe(0).f_code.co_name)
         self.arr: List[UserVO] = []
         self.create_user = CreateUser(TestUserRepositoryList(self.arr))
+        self.login_user = LoginUser(TestUserRepositoryList(self.arr))
+
         self.create_user.create("taks123", "1q2w3e4r!@#$", "takgyun Lee")
         self.create_user.create("hahahoho119", "1b2n3m4!#@", "Ho Han")
         self.create_user.create("mygun7749", "$1#awb5$", "Guna Yoo")
@@ -40,6 +42,62 @@ class test_create_user(unittest.TestCase):
     def tearDown(self):
         "Hook method for deconstructing the test fixture after testing it."
         print("\t", sys._getframe(0).f_code.co_name)
+
+    def test_login_user_로그인_안_되는거_확인(self):
+        print("\t\t", sys._getframe(0).f_code.co_name)
+
+        user = self.login_user.login("tak123", "1q2w3e4r!@#$")
+
+        self.assertDictEqual(
+            {"type": "IDNotFound"},
+            user.__dict__,
+        )
+
+        user = self.login_user.login("hahahoho119", "2n3m4!#@")
+
+        self.assertDictEqual(
+            {"type": "PasswardNotCorrect"},
+            user.__dict__,
+        )
+
+    def test_login_user_잘_로그인_되는거_확인(self):
+        print("\t\t", sys._getframe(0).f_code.co_name)
+
+        user = self.login_user.login("taks123", "1q2w3e4r!@#$")
+
+        self.assertDictEqual(
+            {
+                "user_id": UserId(id="taks123"),
+                "name": "takgyun Lee",
+                "password": Password(pw="1q2w3e4r!@#$"),
+                "uid": Uid(idx=1),
+            },
+            user.__dict__,
+        )
+
+        user = self.login_user.login("hahahoho119", "1b2n3m4!#@")
+
+        self.assertDictEqual(
+            {
+                "user_id": UserId(id="hahahoho119"),
+                "name": "Ho Han",
+                "password": Password(pw="1b2n3m4!#@"),
+                "uid": Uid(idx=2),
+            },
+            user.__dict__,
+        )
+
+        user = self.login_user.login("mygun7749", "$1#awb5$")
+
+        self.assertDictEqual(
+            {
+                "user_id": UserId(id="mygun7749"),
+                "name": "Guna Yoo",
+                "password": Password(pw="$1#awb5$"),
+                "uid": Uid(idx=3),
+            },
+            user.__dict__,
+        )
 
     def test_result(self):
         print("\t\t", sys._getframe(0).f_code.co_name)
@@ -91,6 +149,9 @@ class test_create_user(unittest.TestCase):
             },
             user.__dict__,
         )
+
+    def test_중복_아이디_생성하는지_확인(self):
+        print("\t\t", sys._getframe(0).f_code.co_name)
 
         # 중복 아이디
         ret = self.create_user.create("taks123", "1q2w3e4r!@#$", "takgyun Lee")
