4fa4fc78819f49584c59636f99b45418af19a6b9
taktakgyun <vbmrkk@naver.com>
2023-12-27 01:14:34 +0900

    🎨 style : update code style

--- Infrastructures/MySQL/Storages/MySqlPostStorage.py
+++ Infrastructures/MySQL/Storages/MySqlPostStorage.py
@@ -14,9 +14,9 @@ import pymysql
 
 
 class MySqlPostStorage(IPostRepository):
-    def __init__(self, user_repo:IUserRepository, name_padding: str = "log_"):
+    def __init__(self, user_repo: IUserRepository, name_padding: str = "log_"):
         self.name_padding = name_padding
-        self.user_repo =  user_repo
+        self.user_repo = user_repo
 
     def connect(self):
         from get_db_data import get_mysql_dict
@@ -31,26 +31,35 @@ class MySqlPostStorage(IPostRepository):
             cursorclass=pymysql.cursors.DictCursor,
         )
 
-    
-    def _convert_to_postvo(self, row:dict)->PostVO:
+    def _convert_to_postvo(self, row: dict) -> PostVO:
         match row["user_id"]:
             case user_id if user_id is not None:
                 return PostVO(
                     title=row["title"],
                     content=Content(content=row["content"]),
-                    create_time=PostCreateTime(time=row["create_time"]),#time=datetime.strptime(row["create_time"], '%Y-%m-%d %H:%M:%S')),
-                    update_time=PostUpdateTime(time=row["update_time"]),#time=datetime.strptime(row["update_time"], '%Y-%m-%d %H:%M:%S')),
+                    create_time=PostCreateTime(
+                        time=row["create_time"]
+                    ),  # time=datetime.strptime(row["create_time"], '%Y-%m-%d %H:%M:%S')),
+                    update_time=PostUpdateTime(
+                        time=row["update_time"]
+                    ),  # time=datetime.strptime(row["update_time"], '%Y-%m-%d %H:%M:%S')),
                     post_id=PostId(idx=row["post_id"]),
-                    user=self.user_repo.search_by_uid(Uid(idx=row["user_id"])).get_simple_user()
+                    user=self.user_repo.search_by_uid(
+                        Uid(idx=row["user_id"])
+                    ).get_simple_user(),
                 )
             case _:
                 return PostVO(
                     title=row["title"],
                     content=Content(content=row["content"]),
-                    create_time=PostCreateTime(time=row["create_time"]),#time=datetime.strptime(row["create_time"], '%Y-%m-%d %H:%M:%S')),
-                    update_time=PostUpdateTime(time=row["update_time"]),#time=datetime.strptime(row["update_time"], '%Y-%m-%d %H:%M:%S')),
+                    create_time=PostCreateTime(
+                        time=row["create_time"]
+                    ),  # time=datetime.strptime(row["create_time"], '%Y-%m-%d %H:%M:%S')),
+                    update_time=PostUpdateTime(
+                        time=row["update_time"]
+                    ),  # time=datetime.strptime(row["update_time"], '%Y-%m-%d %H:%M:%S')),
                     post_id=PostId(idx=row["post_id"]),
-                    user=None
+                    user=None,
                 )
 
     def get_padding_name(self, name: str) -> str:
@@ -79,26 +88,35 @@ class MySqlPostStorage(IPostRepository):
             with connection.cursor() as cursor:
                 match post.user:
                     case user if isinstance(user, SimpleUser):
-                        insert_query = (
-                            f"INSERT INTO {table_name} (title, content, user_id, create_time, update_time) VALUES (%s, %s, %s, %s, %s);"
-                        )
+                        insert_query = f"INSERT INTO {table_name} (title, content, user_id, create_time, update_time) VALUES (%s, %s, %s, %s, %s);"
                         cursor.execute(
                             insert_query,
-                            (post.title, post.content.content, post.user.uid.idx, post.create_time.get_time(), post.update_time.get_time()),
+                            (
+                                post.title,
+                                post.content.content,
+                                post.user.uid.idx,
+                                post.create_time.get_time(),
+                                post.update_time.get_time(),
+                            ),
                         )
                     case none if none is None:
-                        insert_query = (
-                            f"INSERT INTO {table_name} (title, content, create_time, update_time) VALUES (%s, %s, %s, %s);"
-                        )
+                        insert_query = f"INSERT INTO {table_name} (title, content, create_time, update_time) VALUES (%s, %s, %s, %s);"
                         cursor.execute(
                             insert_query,
-                            (post.title, post.content.content, post.create_time.get_time(), post.update_time.get_time()),
+                            (
+                                post.title,
+                                post.content.content,
+                                post.create_time.get_time(),
+                                post.update_time.get_time(),
+                            ),
                         )
                     case d if isinstance(d, dict):
-                        return Fail(type="Fail_Mysql_SavePost_ValueError(user.type==dict)")
+                        return Fail(
+                            type="Fail_Mysql_SavePost_ValueError(user.type==dict)"
+                        )
                     case _:
                         return Fail(type="Fail_Mysql_SavePost_unknown")
-                # 마지막 실행 파일 찾기 
+                # 마지막 실행 파일 찾기
                 sql_query = f"SELECT * FROM {table_name} ORDER BY post_id DESC LIMIT 1;"
                 cursor.execute(sql_query)
 
@@ -106,7 +124,7 @@ class MySqlPostStorage(IPostRepository):
                 result = cursor.fetchone()
 
                 if result:
-                    ret = self._convert_to_postvo(result) 
+                    ret = self._convert_to_postvo(result)
                 else:
                     return Fail(type="Fail_Mysql_SavePost_GetFailID")
             connection.commit()
@@ -121,9 +139,8 @@ class MySqlPostStorage(IPostRepository):
                 return ret.get_simple_post()
             case _:
                 return Fail(type="Fail_Mysql_SavePost_NotSavedPost")
-    
-    
-    def _get_post_per_page_list_all(self)-> Result[Collection[SimplePost]]:
+
+    def _get_post_per_page_list_all(self) -> Result[Collection[SimplePost]]:
         connection = self.connect()
         table_name = self.get_padding_name("post")
 
@@ -149,22 +166,26 @@ class MySqlPostStorage(IPostRepository):
         posts_per_page: Optional[int] = None,
     ) -> Result[Collection[SimplePost]]:
         match posts_per_page:
-            case _  if posts_per_page is not None:
-                pass # 아래에서 처리
+            case _ if posts_per_page is not None:
+                pass  # 아래에서 처리
             case _ if posts_per_page is None:
                 return self._get_post_per_page_list_all()
             case _ if posts_per_page < 1:
-                raise ValueError(f"Must posts_per_page >= 1, current value({posts_per_page})")
-        
+                raise ValueError(
+                    f"Must posts_per_page >= 1, current value({posts_per_page})"
+                )
+
         connection = self.connect()
         table_name = self.get_padding_name("post")
 
         # This query will return a list of post_ids in descending order, limited by posts_per_page and offset by page*posts_per_page
-        select_query = f"SELECT * FROM {table_name} ORDER BY post_id DESC LIMIT %s OFFSET %s;"
+        select_query = (
+            f"SELECT * FROM {table_name} ORDER BY post_id DESC LIMIT %s OFFSET %s;"
+        )
         try:
             with connection.cursor() as cursor:
                 # Execute the query with the given parameters
-                cursor.execute(select_query, (posts_per_page, (page)*posts_per_page))
+                cursor.execute(select_query, (posts_per_page, (page) * posts_per_page))
                 # Fetch all the results of the query
                 results = cursor.fetchall()
         except Exception as ex:
@@ -176,7 +197,6 @@ class MySqlPostStorage(IPostRepository):
         return [self._convert_to_postvo(row).get_simple_post() for row in results]
 
     def search_by_pid(self, post_id: PostId) -> Optional[PostVO]:
-        from icecream import ic
         connection = self.connect()
         table_name = self.get_padding_name("post")
         try:
@@ -193,18 +213,20 @@ class MySqlPostStorage(IPostRepository):
             connection.close()
 
     def search_by_uid(
-        self, 
+        self,
         user_id: Uid,
         page: int = 0,
         posts_per_page: Optional[int] = None,
-        ) -> Result[Collection[SimplePost]]:
+    ) -> Result[Collection[SimplePost]]:
         connection = self.connect()
         table_name = self.get_padding_name("post")
 
         select_query = f"SELECT * FROM {table_name} WHERE user_id = %s ORDER BY post_id DESC LIMIT %s OFFSET %s;"
         try:
             with connection.cursor() as cursor:
-                cursor.execute(select_query, (user_id.idx, posts_per_page, (page)*posts_per_page))
+                cursor.execute(
+                    select_query, (user_id.idx, posts_per_page, (page) * posts_per_page)
+                )
                 results = cursor.fetchall()
         except:
             connection.rollback()
@@ -215,11 +237,11 @@ class MySqlPostStorage(IPostRepository):
         return [self._convert_to_postvo(row).get_simple_post() for row in results]
 
     def search_by_query(
-        self, 
+        self,
         query: IConditionRanderForPostQuery,
         page: int = 0,
         posts_per_page: Optional[int] = None,
-        ) -> Result[Collection[SimplePost]]:
+    ) -> Result[Collection[SimplePost]]:
         raise NotImplementedError()
 
     def update(self, post: PostVO) -> Result[SimplePost]:
@@ -229,7 +251,15 @@ class MySqlPostStorage(IPostRepository):
         update_query = f"UPDATE {table_name} SET title = %s, content = %s, update_time = %s WHERE post_id = %s;"
         try:
             with connection.cursor() as cursor:
-                cursor.execute(update_query, (post.title, post.content.content, post.update_time.get_time(), post.post_id.idx))
+                cursor.execute(
+                    update_query,
+                    (
+                        post.title,
+                        post.content.content,
+                        post.update_time.get_time(),
+                        post.post_id.idx,
+                    ),
+                )
                 connection.commit()
         except Exception as ex:
             connection.rollback()
